b537aaabdc36227e1f27a697a93189af788f3100
barunslick <barunpradhan987@gmail.com>
2020-07-29 11:32:44 +0545

    Updates

---
+++ precache-manifest.308c9fb87eed64bf1ea3d2dc37fe986b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1c32b0a17d562ee0ac420255d06e30c",
+    "url": "/lazy-beaver/index.html"
+  },
+  {
+    "revision": "3c83e76defe67de6d55d",
+    "url": "/lazy-beaver/static/css/main.e7ed9830.chunk.css"
+  },
+  {
+    "revision": "29c2b004b7cba34b5bea",
+    "url": "/lazy-beaver/static/js/2.7cc34e92.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lazy-beaver/static/js/2.7cc34e92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c83e76defe67de6d55d",
+    "url": "/lazy-beaver/static/js/main.40c3a13c.chunk.js"
+  },
+  {
+    "revision": "13f98aa25d9a88ade504",
+    "url": "/lazy-beaver/static/js/runtime-main.1c4f483a.js"
+  }
+]);
